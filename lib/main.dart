c13d01ecd42609d595ba2f9bef84d24f431c0d44
Baloch Script <soltansalim97@gmail.com>
2026-06-27 17:20:10 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart'; // Fixed import typo (.dart added!)
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // FIXED: Added missing import statement!
 import 'balochi_keyboard_config.dart';
 
 void main() {
